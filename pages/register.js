b58fffac00c6538807352e8575d30f5fad9b97e3
Giancarlo Guerra <gian.guerra23@gmail.com>
2021-08-26 16:05:03 -0500

    Se implemento la pagina de registro y sus funcionalidades

--- pages/register.js
+++ pages/register.js
@@ -6,54 +6,67 @@ import {
   Button,
   Link,
 } from "@material-ui/core";
-import NextLink from "next/link";
-import { useRouter } from 'next/router';
-import Layout from "../components/Layout";
-import useStyles from "../utils/styles";
 import axios from "axios";
-import React, { useContext, useEffect,useState } from "react";
-import { Store } from '../utils/Store';
-import Cookies from 'js-cookie';
+import { useRouter } from "next/router";
+import NextLink from "next/link";
+import React, { useContext, useEffect, useState } from "react";
+import Layout from "../components/Layout";
+import { Store } from "../utils/Store";
+import useStyles from "../utils/styles";
+import Cookies from "js-cookie";
 
-export default function Login() {
-
+export default function Register() {
   const router = useRouter();
-  const { redirect } = router.query; // login?redirect=/shipping
+  const { redirect } = router.query;
   const { state, dispatch } = useContext(Store);
   const { userInfo } = state;
-  //If user is logged in, is not necessary to login anymore.
   useEffect(() => {
     if (userInfo) {
-      router.push('/');
+      router.push("/");
     }
   }, []);
 
-
+  const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
   const classes = useStyles();
-
   const submitHandler = async (e) => {
     e.preventDefault();
+    if (password !== confirmPassword) {
+      alert("passwords don't match");
+      return;
+    }
     try {
-      const { data } = await axios.post("/api/users/login", {
+      const { data } = await axios.post("/api/users/register", {
+        name,
         email,
         password,
       });
-      dispatch({ type: 'USER_LOGIN', payload: data });
-      Cookies.set('userInfo', data);
-      router.push(redirect || '/');
+      dispatch({ type: "USER_LOGIN", payload: data });
+      Cookies.set("userInfo", data);
+      router.push(redirect || "/");
     } catch (err) {
       alert(err.response.data ? err.response.data.message : err.message);
     }
   };
   return (
-    <Layout title="Login">
-      <form className={classes.form} onSubmit={submitHandler}>
+    <Layout title="Register">
+      <form onSubmit={submitHandler} className={classes.form}>
         <Typography component="h1" variant="h1">
-          Login
+          Register
         </Typography>
         <List>
+          <ListItem>
+            <TextField
+              variant="outlined"
+              fullWidth
+              id="name"
+              label="Name"
+              inputProps={{ type: "text" }}
+              onChange={(e) => setName(e.target.value)}
+            ></TextField>
+          </ListItem>
           <ListItem>
             <TextField
               variant="outlined"
@@ -75,14 +88,24 @@ export default function Login() {
             ></TextField>
           </ListItem>
           <ListItem>
+            <TextField
+              variant="outlined"
+              fullWidth
+              id="confirmPassword"
+              label="Confirm Password"
+              inputProps={{ type: "password" }}
+              onChange={(e) => setConfirmPassword(e.target.value)}
+            ></TextField>
+          </ListItem>
+          <ListItem>
             <Button variant="contained" type="submit" fullWidth color="primary">
-              Login
+              Register
             </Button>
           </ListItem>
           <ListItem>
-            Do not have an account? &nbsp;
-            <NextLink href={`/register?redirect=${redirect || '/'}`} passHref>
-              <Link>Register</Link>
+            Already have an account? &nbsp;
+            <NextLink href={`/login?redirect=${redirect || "/"}`} passHref>
+              <Link>Login</Link>
             </NextLink>
           </ListItem>
         </List>
